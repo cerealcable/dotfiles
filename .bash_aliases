a503cbadd4e8da96fa44efefd6b7dc1d183833a1
Morgan Humes <morgan@lanaddict.com>
2017-05-18 14:28:57 -0500

    Support "environments"

--- .bash_aliases
+++ .bash_aliases
@@ -2,9 +2,10 @@
 SCREEN_FILES=`ls -a ~/Projects/*/.screenrc-* ~/.screenrc-* 2>/dev/null`
 for FILE in $SCREEN_FILES;
 do
-        NAME=`echo $FILE | rev | cut -d- -f1 | rev`;
-        alias $NAME="screen -c $FILE"
+    NAME=`echo $FILE | rev | cut -d- -f1 | rev`;
+    alias $NAME="screen -c $FILE"
 done
+
 
 # Static aliases
 alias ssh-adtran='ssh -oKexAlgorithms=+diffie-hellman-group1-sha1 -oHostKeyAlgorithms=+ssh-dss'
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+SOURCE_ENV=""
+
+while getopts ":e:" opt; do
+  case $opt in
+    e)
+      if [ -d envs/$OPTARG ]; then
+          SOURCE_ENV="envs/$OPTARG";
+          echo "Environment is $OPTARG";
+      else
+          echo "Invalid Environment, must be a directory!";
+          exit 2;
+      fi;
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2;
+      exit 1;
+      ;;
+  esac
+done
 
 git pull origin master;
 
@@ -12,7 +30,11 @@ function doIt() {
           --exclude "README.md" \
           --exclude "LICENSE-MIT.txt" \
           --exclude "requirements-user.txt" \
+          --exclude "envs" \
           -avh --no-perms . ~;
+    if [ $SOURCE_ENV != "" ]; then
+        rsync -avh --no-perms $SOURCE_ENV/ ~;
+    fi;
     pip install --user -r requirements-user.txt
     source ~/.bash_profile;
 }
